59a64d8394ed579e54275672e743b137c8e51524
幸运 <lzr52o@qq.com>
2026-03-23 16:14:44 +0800

    Create d1.js

---
+++ src/utils/d1.js
@@ -0,0 +1,10 @@
+export async function initDatabase(env) {
+  await env.DB.exec(`
+    CREATE TABLE IF NOT EXISTS modules (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT UNIQUE,
+      size INTEGER,
+      created_at TEXT
+    );
+  `);
+}
